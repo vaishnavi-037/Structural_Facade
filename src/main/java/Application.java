e7b11ce6bfbc01789aff5b4a22f9c6a7ec19ea12
Vaishnavi Gupta <muskan.vgupta306@gmail.com>
2024-10-30 20:10:37 +0530

    Add examples for facade design pattern

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+import facade.VideoConversionFacade;
+
+import java.io.File;
+
+public class Application {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+    }
+}
